08dca2f8cdc5ff3347abedca49c1613d4326a01a
amine <amine.reda037@gmail.com>
2021-02-27 16:20:44 +0100

    add

--- src/main/java/org/sid/entity/Wallet.java
+++ src/main/java/org/sid/entity/Wallet.java
@@ -1,5 +1,6 @@
 package org.sid.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -9,17 +10,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
 import javax.persistence.PrePersist;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,31 +28,28 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 public class Wallet {
-@Id	
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
-@NotNull(message = "Name can't be blank")
-@Size(min=2,max=30)
-private String name;
-@Size(max=30)
-private String accountNumber;
-@Size(max=100)
-private String description;
-@Min(1)
-@Max(3)
-private Integer priority;
-private Double currentBalance;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull(message = "Name can't be blank")
+	@Size(min = 2, max = 30)
+	private String name;
+	@Size(max = 30)
+	private String accountNumber;
+	@Size(max = 100)
+	private String description;
+	@Min(1)
+	@Max(3)
+	private Integer priority;
+	private Double currentBalance;
 
-@OneToMany(cascade = CascadeType.REFRESH,fetch = FetchType.LAZY,mappedBy ="wallet",orphanRemoval = true )
-@JsonIgnore
-private List<Transaction> transactions;
+	@OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY, mappedBy = "wallet", orphanRemoval = true)
+	@JsonIgnore
+	private List<Transaction> transactions = new ArrayList<>();
 
-@PrePersist
-public void setBalance() {
-	this.currentBalance=new Double(0);
-}
+	@PrePersist
+	public void setBalance() {
+		this.currentBalance = new Double(0);
+	}
 
-
-	
-	
 }
